bd09cba219efa907265d7c2347638bf8f29f9c5f
chauzov <33752744+chauzov@users.noreply.github.com>
2018-03-15 15:55:32 +0700

    Update ex10.py

--- ex10.py
+++ ex10.py
@@ -1,1 +1,40 @@
 # task: write method to generate Marco's replies
+
+# coding=utf-8
+import random
+import os, sys
+
+greeting = """
+Hi,
+
+"""
+
+numberOfPhrases = 5
+
+signature = """
+
+
+John Doe
++1 555 800 3444
+"""
+
+
+dicList = []
+
+with open('john.dic') as a_file:
+    for line in a_file:
+        dicList.append(line)
+
+reply = greeting
+
+for i in range(1, numberOfPhrases):
+    pos = random.randrange(len(dicList))
+    line = dicList[pos]
+    dicList[pos] = dicList[-1]
+    del dicList[-1]
+    line = line.rstrip('\n')
+    reply = reply + ' ' + line
+
+reply = reply + signature
+
+print reply
