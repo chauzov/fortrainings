92122c28991402ddb4bc392cc3b0c815a7d99a41
chauzov <33752744+chauzov@users.noreply.github.com>
2018-03-15 11:23:03 +0700

    Update ex3.py

--- ex3.py
+++ ex3.py
@@ -1,2 +1,9 @@
 # task: create empty list, dict, set, tuple, and set
 # prove that this is that type
+
+aList = []
+aDict = {}
+aSet = set()
+aTuple = ()
+
+print "aList: {0}, aDict: {1}, aSet: {2}, aTuple: {3}".format(type(aList),type(aDict),type(aSet),type(aTuple))
